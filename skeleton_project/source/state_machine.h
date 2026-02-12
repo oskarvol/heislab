b85115532924f71f6ed2c3c44c887fe7290c556e
nei <nei@nei.com>
2026-02-12 15:41:15 +0100

    started logic

---
+++ skeleton_project/source/state_machine.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "driver/elevio.h"
+
+struct state {
+    int current_floor;
+    int motor_dir;
+    bool door_open;
+
+};
+
+bool sm_init(bool motor_running, int door_open, bool at_floor);
+void sm_update(bool motor_running, bool door_open, bool at_floor);
+
+
