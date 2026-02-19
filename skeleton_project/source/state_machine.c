363b7c92f95991f1ccb7f780dceb61605e0d039f
sanntid_pc <sanntid_pc@sanntid.com>
2026-02-19 13:24:36 +0100

    Workin sm_init

--- skeleton_project/source/state_machine.c
+++ skeleton_project/source/state_machine.c
@@ -5,15 +5,31 @@
 
 bool stop_signal = false;
 struct state current_state;
-bool sm_init(bool motor_running, int door_open, bool at_floor) {
-    if (door_open) {        
-        elevio_doorOpenLamp(0);    
-    }    
-    current_state.door_open = false;
-    while (!at_floor && elevio_stopButton() == 0){ 
-        elevio_motorDirection(-1);    
+bool sm_init() {
+    int floor;
+    elevio_motorDirection(0);
+    current_state.motor_dir = 0;
+    current_state.current_floor = elevio_floorSensor();
+    elevio_doorOpenLamp(0); 
+
+    while (current_state.current_floor == -1 && elevio_stopButton() == 0){ 
+        elevio_motorDirection(-1);
+        current_state.current_floor = elevio_floorSensor();
+        current_state.motor_dir = -1;    
+        floor = elevio_floorSensor();
+        printf("utenfor floor\n");
     };
-    elevio_motorDirection(0);   
-    current_state.current_floor = elevio_floorSensor();    
-    return true;
+    printf("floor \n");
+    elevio_motorDirection(0);
+    current_state.motor_dir = 0;
+
+
+    if (current_state.current_floor != -1){
+        current_state.current_floor = elevio_floorSensor();
+        printf("ferdig\n");
+        return true;
+    };
+    
+    sm_init();
+    return false;
 };
--- skeleton_project/source/state_machine.h
+++ skeleton_project/source/state_machine.h
@@ -5,10 +5,10 @@
 struct state {
     int current_floor;
     int motor_dir;    
-    bool door_open;
+
 };
 
-bool sm_init(bool motor_running, int door_open, bool at_floor);
-void sm_update(bool motor_running, bool door_open, bool at_floor);
+bool sm_init(void);
+void sm_update(void);
 
 
